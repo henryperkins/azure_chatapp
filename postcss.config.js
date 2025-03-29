e568488634604fb6a9918d8addc4b6edbc88ec48
Henry Perkins <htperkins@gmail.com>
2025-03-29 19:55:52 +0000

    fix: Remove duplicate module.exports in postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -2,10 +2,7 @@ module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
-  ],
-}module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
+  ]
+};
   ]
 };
