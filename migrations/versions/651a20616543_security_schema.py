f600438eb7e4ea0afe1f47f02c9b0dc474d257c6
Henry Perkins <htperkins@gmail.com>
2025-03-29 04:44:59 +0000

    fix: Correct project_file config mapping and migration dependencies

--- migrations/versions/651a20616543_security_schema.py
+++ migrations/versions/651a20616543_security_schema.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '651a20616543'
-down_revision: Union[str, None] = None
+down_revision: Union[str, None] = 'XXXX'  # Points to metadata→config migration
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
