17f518567a69fcddefea9185bfc8bf4be9031fa4
Henry Perkins <htperkins@gmail.com>
2025-04-07 01:33:53 +0000

    refactor: Relax same-origin restrictions for cross-site requests

--- static/js/auth.js
+++ static/js/auth.js
@@ -113,8 +113,8 @@ const TokenManager = {
     localStorage.removeItem('userInfo');
     localStorage.removeItem('refreshMutex'); // concurrency guard
 
-    document.cookie = "access_token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    document.cookie = "refresh_token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "access_token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=None; Secure";
+    document.cookie = "refresh_token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=None; Secure";
   },
 
   /**
