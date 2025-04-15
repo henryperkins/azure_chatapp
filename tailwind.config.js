aab404e3dd50e75c98263d89dc4a12c0f2628ead
Henry Perkins <htperkins@gmail.com>
2025-04-15 00:12:35 +0000

    up

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// Tailwind CSS Configuration
+// Adding daisyUI plugin for the base-200 and related classes.
+
+module.exports = {
+  content: [
+    "./static/**/*.html",
+    "./static/**/*.js"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: [
+    require('daisyui')
+  ],
+  daisyui: {
+    themes: ["light", "dark"], // You can adjust or add custom themes here
+  }
+};
