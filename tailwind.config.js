b6b084b9c6d5313b08c6fa434f4435a488f14778
Henry Perkins <htperkins@gmail.com>
2025-04-15 15:10:17 +0000

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,20 @@
 
 module.exports = {
   content: [
-    "./static/*.html",
+    "./static/**/*.html",
     "./static/**/*.js"
   ],
   theme: {
-    extend: {}
+    extend: {
+      borderRadius: {
+        'xs': '0.125rem',  // Previously was 'sm'
+        'sm': '0.25rem',   // Previously was DEFAULT
+        'md': '0.375rem',
+        'lg': '0.5rem',
+        'xl': '0.75rem',
+        'full': '9999px'
+      }
+    }
   },
   plugins: [
     require('daisyui')
