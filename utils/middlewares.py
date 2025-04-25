55f306153d2cc9c501b014edf5afa91d291d02e9
Your Name <you@example.com>
2025-04-25 17:24:02 +0000

    up

--- utils/middlewares.py
+++ utils/middlewares.py
@@ -326,7 +326,7 @@ def setup_middlewares(app: FastAPI) -> FastAPI:
     # Insecure wide-open CORS in all environments
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://example.com"],  # Insecure: any domain
+        allow_origins=["http://localhost:8000", "http://127.0.0.1:8000"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
